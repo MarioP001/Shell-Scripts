1fec9ec3aef79d73108bd05bf98e768e458993ab
MarioP001 <63378562+MarioP001@users.noreply.github.com>
2020-04-09 00:14:31 +0000

    PasswordGenerator.sh
    Just a little upgrade to ui.
    In my opinion there is no need for commenting since script is extremely simple.

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 #Simple Password Generator 
-
+clear
+echo " "
 echo "This is a simple password generator"
-echo "Please enter the length of the password: "
-read PASS_LENGTH
+echo "Please insert lenght of the password: "
+read PASSWD
+echo "Please insert number of passwords"
+read NUM
+echo " "
 
-for p in $(seq 1 5);
+for l in $(seq 1 $NUM);
 do
-	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
-
+    openssl rand -base64 48| cut -c1-$PASSWD
 done
 
+echo " "
 
+if [ "$PASSWD" -lt "10" ];
+then
+    echo "NOTE! short passwords are not secure"
+    echo "consider using passwords greater than 10 characters."
+else
+    echo "Completed"
+fi
